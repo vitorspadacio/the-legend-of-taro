4577a65516c2a3e51edae8bb18a5a1f025a623f3
Vitor Spadacio Pisani <vsp193@gmail.com>
2026-08-01 12:08:43 -0300

    teste de morte do inimigo

--- enemy.gd
+++ enemy.gd
@@ -2,8 +2,15 @@ extends Sprite2D
 
 @onready var damage_area: DamageArea = $DamageArea
 
+var hp := 5.0
+
 func _ready() -> void:
 	damage_area.damage_taken.connect(_on_damage_taken)
 
+func _process(_delta: float) -> void:
+	if hp <= 0:
+		queue_free()
+
 func _on_damage_taken(attack_area: AttackArea) -> void:
-	print("Tomou dano")
+	hp -= attack_area.damage
+	print("Tomou dano. HP total: %s" % hp)
